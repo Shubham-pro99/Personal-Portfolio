1b5b05a11ce872d3993157ccc0c4b22ec48550cd
Shubham Redij <117507738+Shubham-pro99@users.noreply.github.com>
2024-10-02 09:21:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './Component/NavBar';
+import Banner from './Component/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Banner.js
@@ -0,0 +1,18 @@
+import { Container,Row,Col } from "react-bootstrap";
+import '../Component-Styles/banner.css'
+
+function Banner() {
+    return(
+<section className="banner" id="home">
+    <Container>
+        <Row className="align-items-center">
+            <Col xs={12} md={12} lg={7}>
+               
+            </Col>
+        </Row>
+    </Container>
+</section>
+    )
+}
+
+export default Banner;
